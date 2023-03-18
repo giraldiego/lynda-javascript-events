d50c94026278292d35a0ca83cb349d93bc1875dd
giraldiego <giraldiego@gmail.com>
2023-03-18 03:38:07 -0500

    Start 02_03 - Creating DOM elements

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-document.querySelector(".grid").addEventListener("click", function (e) {
-  const tgt = e.target;
-  if (tgt.tagName === "IMG") {
-    // only if the clicked element is an image
-    const parent = tgt.parentNode; // li
-    const grandParent = parent.parentNode; // ul
-    // console.log(tgt.alt);
-    // console.log(grandParent.children[0].children[0].alt);
-    if (grandParent.children.length > 1) {
-    //   console.dir(grandParent);
-      grandParent.removeChild(parent);
-    }
-    if (grandParent.children.length === 1) {
-      const lastAlt = grandParent.children[0].children[0].alt; 
-      document.querySelector("#art p").innerHTML = lastAlt;
-    }
-  }
-});
+document.querySelector('.grid').addEventListener('mouseover', function(e) {
+  if (e.target.tagName === 'IMG') {
+
+
+  } // check to see that I clicked on IMG only
+}, false); // click event
